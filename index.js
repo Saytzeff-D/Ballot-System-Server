0ba37ee97cd18b3ae6ed22c3e1d2489882b798d2
David Atanda <ololadedavid15@gmail.com>
2023-03-07 14:26:54 +0100

    config cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 5200
 const bodyParser = require("body-parser");
+const AccountRouter = require('./routes/account');
+
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors({origin: '*'}))
+app.use('/account', AccountRouter)
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Ballot Server listening on port ${port}!`))
+app.listen(port, () => console.log(`Ballot Server listening on port ${port}`))

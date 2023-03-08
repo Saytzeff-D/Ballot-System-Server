2f54a79671e1cb3506896cdc2f03e2511378e13e
David Atanda <ololadedavid15@gmail.com>
2023-03-08 17:58:40 +0100

    now inserting users into sql db

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,5 +1,29 @@
+const uuid = require('uuid');
+const pool = require('../connections/pool');
+
 const register = (req, res)=>{
-    res.json('Delivered')
+    let user = req.body
+    user.ballotKey = uuid.v4()
+    // res.json(user)
+    let sql = `SELECT * FROM users WHERE email = '${req.body.email}'`
+    pool.query(sql, (err, result)=>{
+        if(err){
+            throw err
+        }else{
+            if(result.length == 0){
+                let insertQuery = `INSERT INTO users (fullName, email, password, orgName, ballotKey) VALUES ('${user.fullName}', '${user.email}', '${user.password}', '${user.orgName}', '${user.ballotKey}')`;
+                pool.query(insertQuery, (err, result)=>{
+                    if (err) {
+                        throw err
+                    } else {
+                        res.json({msg: 'Success'})
+                    }
+                })
+            }else{
+                res.status(200).json({msg: 'E-mail Already exists'})
+            }
+        }
+    })
 }
 
 let AccountController = { register }

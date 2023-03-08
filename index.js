d2c7392644d11b3517cb80740ce94e34306d4668
David Atanda <ololadedavid15@gmail.com>
2023-03-08 17:57:47 +0100

    activate pool connection

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const cors = require('cors');
-const port = 5200
+const port = process.env.PORT
 const bodyParser = require("body-parser");
 const AccountRouter = require('./routes/account');
+const pool = require('./connections/pool');
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors({origin: '*'}))
 app.use('/account', AccountRouter)
 
+pool.getConnection((err, conn)=>{
+    if(!err){
+        console.log('SQL Server connected')
+    }else{
+        console.log('Connection Error', err)
+    }
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Ballot Server listening on port ${port}`))
